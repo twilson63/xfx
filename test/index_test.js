313457ab4cf75e637a68e47d757feea76f2a75ce
Tom Wilson <tom@jackhq.com>
2015-08-01 20:34:13 -0400

    added travis

--- test/index_test.js
+++ test/index_test.js
@@ -21,19 +21,19 @@ test('app should fail if no render method on main method', function (t) {
   }
 })
 
-test('app success', function (t) {
-  var main = function (state, update) {
-    return state
-  }
-  main.render = function () {
-    return null
-  }
-
-  try {
-    app(main)
-  } catch (e) {
-    console.log(e)
-  } finally {
-    t.end()
-  }
-})
+// test('app success', function (t) {
+//   var main = function (state, update) {
+//     return state
+//   }
+//   main.render = function () {
+//     return null
+//   }
+//
+//   try {
+//     app(main)
+//   } catch (e) {
+//     console.log(e)
+//   } finally {
+//     t.end()
+//   }
+// })
